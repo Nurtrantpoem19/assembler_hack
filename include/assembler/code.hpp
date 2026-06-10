444eef988fc7dae42259d93526f3e75230e5c9e3
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-06-10 11:44:04 -0400

    code module header complete

---
+++ include/assembler/code.hpp
@@ -0,0 +1,15 @@
+#ifndef CODE_HPP
+#define CODE_HPP
+
+#include <stdio.h>
+#include <string>
+
+class Code
+{
+  public:
+    static std::string dest(const std::string &mnemonic);
+    static std::string jump(const std::string &mnemonic);
+    static std::string comp(const std::string &mnemonic);
+};
+
+#endif // CODE_HPP
