d410f496a547f96402072ad266d1bc02ca28ea14
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-06-09 20:49:52 -0400

    initiating parser hpp header file with basic methods and attributes

--- include/assembler/parser.hpp
+++ include/assembler/parser.hpp
@@ -1,7 +1,31 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
 #include <stdio.h>
+#include <fstream>
 
-int main(void){
+using namespace std;
 
-}
 
-hell:
+class Parser
+{
+private:
+	std::fstream p;
+	std::string currentCommand;
+public:
+	Parser(std::string asm_file);
+	bool hasMoreCommands();
+	void advance();
+	enum class CommandType(A_Command, C_Command, L_Command);
+	CommandType commandType();
+	const std::string symbol(){
+
+	};
+	const std::string dest();
+	const std::string comp();
+	const std::string jump();
+
+
+};
+
+#endif
