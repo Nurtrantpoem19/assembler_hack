f59431e4873335d5fd688e31d3639350807770e3
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-06-12 13:30:23 -0400

    reformat commandtype function

--- include/assembler/parser.hpp
+++ include/assembler/parser.hpp
@@ -10,22 +10,23 @@ class Parser
 {
   private:
     std::ifstream p;
-    std::string currentCommand
+    std::string currentCommand;
 
-	public : Parser(const std::string asm_file);
+  public:
+    Parser(const std::string asm_file);
     void advance();
     enum class CommandType
     {
-	A_Command,
-	C_Command,
-	L_Command,
-	Not_Command
+        A_Command,
+        C_Command,
+        L_Command,
+        Not_Command
     };
     CommandType commandType() const;
-    std::string symbol() const;
-    std::string dest() const;
-    std::string comp() const;
-    std::string jump() const;
+    std::optional<std::string> symbol() const;
+    std::optional<std::string> dest() const;
+    std::optional<std::string> comp() const;
+    std::optional<std::string> jump() const;
 };
 
 #endif
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,11 +1,10 @@
 #include "Parser.hpp"
 #include <fstream>
 
-Parser::Parser(std::string asm_file) : p(asm_file)
+Parser::Parser(std::optional < std::string asm_file) : p(asm_file)
 
 {
     std::getline(p, currentCommand);
-    p
 }
 
 void Parser::advance()
@@ -13,21 +12,21 @@ void Parser::advance()
     while (std::getline(p, currentCommand))
     {
 
-	std::size_t slash = currentCommand.find("//");
-	if (slash != std::string::npos)
-	{
-	    currentCommand = currentCommand.substr(0, slash);
-	}
+        std::size_t slash = currentCommand.find("//");
+        if (slash != std::string::npos)
+        {
+            currentCommand = currentCommand.substr(0, slash);
+        }
 
-	currentCommand.erase(
-	    std::remove_if(currentCommand.begin(), currentCommand.end(),
-			   [](unsigned char ch) { return std::isspace(ch); }),
-	    currentCommand.end());
+        currentCommand.erase(
+            std::remove_if(currentCommand.begin(), currentCommand.end(),
+                           [](unsigned char ch) { return std::isspace(ch); }),
+            currentCommand.end());
 
-	if (!currentCommand.empty())
-	{
-	    return true;
-	}
+        if (!currentCommand.empty())
+        {
+            return true;
+        }
     }
     return false;
 };
@@ -36,23 +35,48 @@ Parser::CommandType Parser::commandType() const
 {
     if (currentCommand.empty())
     {
-	return CommandType::Not_Command;
+        return CommandType::Not_Command;
     }
 
     switch (currentCommand[0])
     {
-    case (' '):
-    case ('	'):
-	return CommandType::Not_Command;
+    case ' ':
+    case '\t':
+        return CommandType::Not_Command;
+
     case '/':
-	if (currentCommand[1] == '/')
-	    return CommandType::Not_Command;
-	break;
-    case ('@'):
-	return CommandType::A_Command;
-    case ('('):
-	return CommandType::L_Command;
+        if (currentCommand[1] == '/')
+            return CommandType::Not_Command;
+        break;
+
+    case '@':
+        return CommandType::A_Command;
+
+    case '(':
+        return CommandType::L_Command;
+
     default:
-	return CommandType::C_Command;
+        return CommandType::C_Command;
     }
+}
+
+std::optional<std::string> Parser::symbol() const
+{
+    if (currentCommand[0] == '@')
+        return currentCommand.substr(1);
+
+    if (currentCommand.front() == '(' && currentCommand.back() == ')')
+        return currentCommand.substr(1, currentCommand.size() - 2);
+
+    return std::nullopt;
+}
+
+std::optional<std::string> Parser::dest() const
+{
+    std::size_t equal_sign = currentCommand.find('=');
+    if (equal_sign == std::string::npos)
+    {
+        return std::nullopt;
+    }
+    return currentCommand.substr(0, equal_sign);
 }
