74fbcacc17fe4e95dee94760c8ff08de597e3266
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-06-15 14:22:37 -0400

    fixup

---
+++ include/assembler/Parser.hpp
@@ -0,0 +1,33 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include <fstream>
+#include <optional>
+#include <stdio.h>
+
+using namespace std;
+
+class Parser
+{
+  private:
+    std::ifstream p;
+    std::string currentCommand;
+
+  public:
+    Parser(const std::string asm_file);
+    bool advance();
+    enum class CommandType
+    {
+        A_Command,
+        C_Command,
+        L_Command,
+        Not_Command
+    };
+    CommandType commandType() const;
+    std::optional<std::string> symbol() const;
+    std::optional<std::string> dest() const;
+    std::string comp() const;
+    std::optional<std::string> jump() const;
+};
+
+#endif
