0b33055fb011f377194086ab7a5f513fed075b00
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-06-17 17:56:51 -0400

    replace hashmap with unordered map

--- include/assembler/symbolTable.hpp
+++ include/assembler/symbolTable.hpp
@@ -1,19 +1,19 @@
 #ifndef SYMBOLTABLE_HPP
 #define SYMBOLTABLE_HPP
 
-#include "HashTable.hpp"
 #include <cstdint>
 #include <optional>
 #include <string>
+#include <unordered_map>
 
 class SymbolTable
 {
   private:
-    HashTable table;
+    std::unordered_map<std::string, uint16_t> symbolTable;
 
   public:
     SymbolTable();
-    void addEntry(const std::string &symbol, std::uint16_t address);
+    void addEntry(const std::string &symbol, uint16_t address);
     bool contains(const std::string &symbol) const;
     std::optional<std::uint16_t> getAddress(const std::string &symbol) const;
 };
